e9f1faf4f62c389edc8293bb86af0c4d2597a5af
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-05-20 21:19:46 +0800

    Create maximusone.js

---
+++ maximusone.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://www.achievablecpmrevenue.com/n3rygrvt?key=a754d241514b7a41541b5461ce5e5e4e'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
