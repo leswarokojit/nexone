2aab61c03a15f8e2857c26ff2021f53c7605caf1
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-09-19 18:28:10 +0800

    Delete mizone1.js

--- mizone1.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-
-  'https://wonderfullytumultbizarre.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
