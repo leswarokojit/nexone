61ee294b78075cfec578a0b902f390434a080479
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2021-09-19 18:04:29 +0800

    Delete maximusone.js

--- maximusone.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-
-  'https://tripsthorpelemonade.com/n3rygrvt?key=a754d241514b7a41541b5461ce5e5e4e'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
