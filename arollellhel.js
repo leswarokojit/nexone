96b383350fa2b58f95a0df5576bcd28b544d9bf7
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2022-01-29 19:42:33 +0800

    Update arollellhel.js

--- arollellhel.js
+++ arollellhel.js
@@ -1,5 +1,6 @@
 var dataUrl = [
-  'https://dev-nexone.pantheonsite.io'
+
+  'https://engagedpungentrepress.com/i8qigyrv?key=489a4f2de7d0a6399f00397a1ce9becb'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
